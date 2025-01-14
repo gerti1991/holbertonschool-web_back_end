3dabffae38c4004acff1ae2f681bab476cd540ee
gerti1991 <gskerja@gmail.com>
2025-01-14 21:01:14 +0100

    10 -Mongo

---
+++ NoSQL/10-update_topics.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+""" List all documents in Python """
+import pymongo
+
+
+def update_topics(mongo_collection, name, topics):
+    """ updates a document based on name """
+    if mongo_collection is None:
+        return None
+    return mongo_collection.update
+    ({"name": name}, {"$set": {"topics": topics}})
