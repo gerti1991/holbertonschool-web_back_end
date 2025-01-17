db109f7fdebf09824322267a8a4894888adb6490
gerti1991 <gskerja@gmail.com>
2025-01-17 15:43:51 +0100

    102 -Mongo

--- NoSQL/102-log_stats.py
+++ NoSQL/102-log_stats.py
@@ -6,13 +6,21 @@ from pymongo import MongoClient
 
 
 def log_stats():
-    """
-    Test
-    """
     client = MongoClient('mongodb://127.0.0.1:27017')
     db = client.logs
     collection = db.nginx
     total_logs = collection.count_documents({})
+    if total_logs == 0:
+        print("0 logs")
+        print("Methods:")
+        print("    method GET: 0")
+        print("    method POST: 0")
+        print("    method PUT: 0")
+        print("    method PATCH: 0")
+        print("    method DELETE: 0")
+        print("0 status check")
+        print("IPs:")
+        return
     methods = collection.aggregate([
         {
             '$group': {
@@ -40,7 +48,6 @@ def log_stats():
     for method in methods:
         print(f"Method {method['_id']}: {method['count']}")
     print(f"{status_check} status check")
-
     print("IPs:")
     for ip in ip_counts:
         print(f"    {ip['_id']}: {ip['count']}")
